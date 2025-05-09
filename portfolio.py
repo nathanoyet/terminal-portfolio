cc039cb9f7d88a3ad2aa15b92e4e1377218963f3
nathanoyet <oyetnathan5@gmail.com>
2025-05-09 18:06:04 +1000

    Initial commit

---
+++ portfolio.py
@@ -0,0 +1,179 @@
+"""
+This python file includes the Portfolio class which is used to create and modify a user's portfolio
+This file also contains global functions that are used in project.py
+"""
+
+from datetime import datetime
+import time
+import random
+
+class Portfolio:
+    def __init__(self, cash):
+        """This function initialises a 2 dictionaries that will store a portfolio of 
+        stocks and all trades made. The starting cash balance provided by the user is
+        also stored
+        """
+        self.portfolio = {}
+        self.trades = {}
+        self.cash = cash
+        self.portfolio_value = 0
+
+    def buy(self, ticker, quantity, price):
+        """
+        This function adds a newly purchased stock to the portfolio or increases its
+        quantity if the stock is already in the portfolio
+        """
+        timestamp = datetime.now()
+        timestamp = timestamp.strftime("%Y-%m-%d %H:%M:%S")
+        buy_amount = quantity * price
+        if (buy_amount > self.cash):
+            return False
+        else:
+            if ticker in self.portfolio:
+                self.portfolio[ticker] += quantity
+            else:
+                self.portfolio[ticker] = quantity
+            self.cash -= buy_amount
+            self.trades[timestamp] = [ticker, quantity, buy_amount, "BUY"]
+            return True
+
+    def sell(self, ticker, quantity, price):
+        """
+        This function reduces the quantity of a stock when sold, and adds to the 
+        cash balance
+        """
+        timestamp = datetime.now()
+        timestamp = timestamp.strftime("%Y-%m-%d %H:%M:%S")
+        sell_amount = quantity * price
+        self.portfolio[ticker] -= quantity
+        self.cash += sell_amount
+        self.trades[timestamp] = [ticker, quantity, sell_amount, "SELL"]
+        if (self.portfolio[ticker] == 0):
+                del self.portfolio[ticker]
+
+    def get_cash(self):
+        """
+        This function returns the cash balance
+        """
+        return self.cash
+    
+    def calculate_portfolio_value(self, market):
+        """
+        This function calculates the total value of all stocks in the portfolio
+        """
+        portfolio_value = 0
+        for position in self.portfolio:
+            portfolio_value += (self.portfolio[position] * market[position])
+        self.portfolio_value = portfolio_value
+
+    def display_portfolio_value(self, portfolio_value, market):
+        """
+        This function prints the portfolio value
+        """
+        self.calculate_portfolio_value(market)
+        print()
+        print(" " * 18, "-" * 38)
+        print(" " * 20, end="")
+        print("Portfolio Value", " " * 10, end="")
+        print(f"${portfolio_value:.2f}".rjust(10))
+        print(" " * 18, "-" * 38)
+    
+    def view_portfolio(self, market):
+        """
+        This function displays the current portfolio
+        """
+        position_index = 1
+        print(" " * 18, "-" * 38)
+        print(" " * 20, end="")
+        print("Ticker", " " * 5, end="")
+        print("Quantity", " " * 4, end="")
+        print("Market Value")
+        print(" " * 18, "-" * 38)
+        for position in self.portfolio:
+                print(" " * 20, end="")
+                print(f"{position_index}. ", end="")
+                print(f"{position}", end="")
+                print(f"{self.portfolio[position]}".rjust(10), end="")
+                print(" " * 9, end="")
+                print(f"${(market[position] * self.portfolio[position]):.2f}".rjust(10))
+                position_index += 1
+        self.display_portfolio_value(self.portfolio_value, market)
+
+    def view_trades(self):
+        """
+        This function displays all trades made
+        """
+        print(" " * 2, "-" * 74)
+        print(" " * 3, end="")
+        print("Timestamp", " " * 15, end="")
+        print("Ticker", " " * 5, end="")
+        print("Action", " " * 5, end="")
+        print("Quantity", " " * 4, end="")
+        print("Trade Amount")
+        print(" " * 2, "-" * 74)
+        for trade in self.trades:   
+            print(" " * 3, end="")
+            print(f"{trade}", " " * 6, end="")
+            print(f"{self.trades[trade][0]}", " " * 5, end="")
+            print(f"{self.trades[trade][3]}".center(8), end="")
+            print(f"{self.trades[trade][1]}".rjust(10), " " * 8, end="")
+            print(f"${self.trades[trade][2]:.2f}".rjust(10))
+
+def refresh_prices(market):
+    """
+    This transaction randomises changes in the market prices of each stock to simulate
+    fluctuations in stock prices
+    """
+    for stock in market:
+        price_change = random.uniform(0.01, 0.10)
+        change_direction = random.randint(1, 2)
+        if change_direction == 1:
+            market[stock] = market[stock] * (1 + price_change)
+        else:
+            market[stock] = market[stock] * (1 - price_change)
+
+
+def view_market(market):
+    """ 
+    This function displays the stocks available for trading along with their market 
+    prices
+    """
+    print(" " * 20, "-" * 30)
+    print(" " * 21, "Stock", " " * 9, end="")
+    print("Current Price")
+    print(" " * 20, "-" * 30)
+    for stock in market:
+        time.sleep(0.05)
+        print(" " * 22, end="")
+        print(f"{stock}", end="")
+        print(" " * 12, end="")
+        print(f"${market[stock]:.2f}".rjust(10))
+
+
+def list_commands(portfolio_name):
+    """
+    This function list all the commands that can be executed 
+    """
+    print(f"\nManage your {portfolio_name} portfolio by entering one of the following commands:\n")
+    time.sleep(0.05)
+    print("1. BUY               -> Buy a stock at the market price")
+    time.sleep(0.05)
+    print("2. SELL              -> Sell a stock at a given price")
+    time.sleep(0.05)
+    print("3. VIEW MARKET       -> View the current prices of available stocks")
+    time.sleep(0.05)
+    print("4. VIEW PORTFOLIO    -> View the stocks in your portfolio")
+    time.sleep(0.05)
+    print("5. VIEW TRADES       -> View your trading history")
+    time.sleep(0.05)
+    print("6. VIEW CASH         -> View your available cash balance")
+    time.sleep(0.05)
+    print("7: VIEW COMMANDS     -> View the list of available commands")
+    time.sleep(0.05)
+    print("8. BACK              -> Go back to the previous command prompt")
+    time.sleep(0.05)
+    print("9. EXIT              -> Exit the program\n")
+
+
+if __name__ == "__main__":
+    pass
---
+++ project.py
@@ -0,0 +1,245 @@
+"""
+** PROJECT DESCRIPTION ** --> include in GitHub readme
+
+This program allows the user to simulate a trading portfolio in the command line terminal.
+Users can only trade stocks that are in a pre-defined list of 10 stocks.
+
+The user begins by giving a name to their portfolio and specifying how much cash they want
+to begin with. The cash entered by the user should be a maximum of $10,000.
+
+The user can then enter one of nine commands to manage their portfolio, view specific data
+and navigate through application. A description of each of the commands is provided below:
+
+- BUY
+Allows a user to specify the ticker symbol and quantity of the stock they would like buy. 
+The stock will be purchased at the current market price. The user's cash balance should be 
+sufficient to complete the purchase.
+
+- SELL
+Allows a user to specify the ticker symbol and quantity of the stock they would like to sell.
+Only stocks currently held in the user's portfolio can be sold. Stocks are sold at the 
+prevailing market price at the time of sale. The proceeds of the sale are added to the user's 
+cash balance.
+
+- VIEW MARKET
+Displays a list of all the stocks and their current market prices.
+
+- VIEW PORTFOLIO
+Displays the user's stock portfolio which includes the stock ticker, the quantity held, and 
+the current market value for each stock. The total portfolio value is also displayed.
+
+- VIEW TRADES
+Displays details on the user's trades. Details include a timestamp, the ticker of the stock 
+traded, the action (buy or sell), the quantity traded and the trade amount in $.
+
+- VIEW CASH
+Displays the user's current cash balance
+
+- VIEW COMMANDS
+Displays the list of commands
+
+- VIEW BACK
+Takes the user to the previous command prompt. If the user is at the base command prompt,
+they cannot go back any further. 
+
+- EXIT
+Prints a goodbye message and the portfolio return. The program is then exited. 
+"""
+
+import portfolio
+
+def main():
+    #initialise the list of tradeable stocks and their market prices
+    market = {
+        "AAPL" : 30,
+        "AMZN" : 13,
+        "GOOG" : 17,
+        "NFLX" : 19,
+        "TSLA" : 8,
+        "NVDA" : 29,
+        "META" : 11,
+        "MSFT" : 6,
+        "ADBE" : 21,
+        "COST" : 4
+    }
+
+    #print welcome message and prompt the user for a valid portfolio name
+    print("-" * 80)
+    print(" " * 20 + "Welcome to the Terminal Portfolio Simulator")
+    print("-" * 80)
+    print()
+    while True:
+        portfolio_name = input(">> What would you like to name your portfolio: ")
+        if len(portfolio_name.split()) < 1:
+            print("\n! Please enter a valid portfolio name !\n")
+        else:
+            break
+
+    while True:
+        #prompt user for the starting cash balance
+        cash = input("\n>> Enter starting cash ($): ")
+        if (cash.isdigit() == False):
+            print("\n! Invalid Cash Amount Entered - Please Try Again !")
+            continue
+        if float(cash) <= 0:
+            print("\n! Invalid Cash Amount Entered - Please Try Again !")
+            continue
+        if float(cash) > 10000:
+            print("\n! Max starting cash allowed is $10,000 !")
+            continue
+        else:
+            cash = float(cash)
+            break
+
+    #create the portfolio project
+    port = portfolio.Portfolio(cash)
+
+    #display available stocks
+    print("\nBelow is the list of stocks available for trading:\n")
+    portfolio.view_market(market)
+    portfolio.list_commands(portfolio_name)
+
+    #main program loop
+    while True:
+        portfolio.refresh_prices(market)
+        command = input(">> Enter a command: ")
+        command = command.upper()
+        print()
+        if command == "BUY":
+            go_back = False
+            while True:
+                #prompt user for valid ticker of stock to be bought
+                buy_ticker = input(">> Enter the ticker of the stock you would like to buy: ").upper()
+                if buy_ticker in market:
+                    break
+                else:
+                    #check if user wants to go back to the previous command prompt
+                    if buy_ticker == "BACK":
+                        go_back = True
+                        print()
+                        break
+                    print("\n! Please enter a valid ticker !\n")
+            #go back to the previous command prompt if specified by the user
+            if go_back:
+                continue
+            while True:
+                #prompt user for quantity to be bought
+                buy_quantity = input(f"\n>> How many shares of {buy_ticker} would you like to buy: ")
+                #check if user wants to go back to the previous command prompt
+                if (buy_quantity.upper() == "BACK"):
+                    go_back = True
+                    print()
+                    break
+                #validate quantity
+                if (buy_quantity.isdigit() == False):
+                    print("\n! Please enter a valid quantity !")
+                    continue
+                if (int(buy_quantity) <= 0):
+                    print("\n! Please enter a valid quantity !")
+                else:
+                    buy_quantity = int(buy_quantity)
+                    break
+            #go back to the previous command prompt if specified by the user
+            if go_back:
+                continue
+            #if the cash balance is sufficient, buy the stock. Print a success message and the cash balance
+            if (port.buy(buy_ticker, buy_quantity, market[buy_ticker])):
+                print()
+                print(f"> {buy_quantity} shares of {buy_ticker} successfully purchased for ${(buy_quantity * market[buy_ticker]):.2f} <".center(80, "-"))
+                print()
+                print(f"Your cash balance is ${port.get_cash():.2f}\n".center(80))
+            else:
+                print(f"\n! Unfortunately, you have insufficient funds to buy {buy_quantity} shares of {buy_ticker} !\n")
+        elif command == "SELL":
+            go_back = False
+            while True:
+                #prompt user for ticker of stock to be sold and verify that its in the portfolio
+                sell_ticker = input(">> Enter the ticker of the stock you would like to sell: ").upper()
+                if sell_ticker in port.portfolio:
+                    break
+                else:
+                    #check if user wants to go back to the previous command prompt
+                    if (sell_ticker == "BACK"):
+                        go_back = True
+                        print()
+                        break
+                    #validate ticker
+                    if (sell_ticker.isdigit() == True or len(sell_ticker.split()) != 1):
+                        print(f"\n! Please provide a valid ticker !\n")
+                    else:
+                        print(f"\n! You don't own any {sell_ticker} !\n")
+            #go back to the previous command prompt if specified by the user
+            if go_back:
+                continue
+            while True:
+                #prompt user for quantity to be sold
+                sell_quantity = input(f"\n>> How many shares of {sell_ticker} would you like to sell: ")
+                #check if user wants to go back to the previous command prompt
+                if (sell_quantity.upper() == "BACK"):
+                    go_back = True
+                    print()
+                    break
+                #validate quantity
+                if (sell_quantity.isdigit() == False):
+                    print("\n! Please enter a valid quantity !")
+                    continue
+                if (int(sell_quantity) <= 0):
+                    print("\n! Please enter a valid quantity !")
+                    continue
+                if (int(sell_quantity) > port.portfolio[sell_ticker]):
+                    print(f"\n! You only have {port.portfolio[sell_ticker]} shares of {sell_ticker} to sell !")
+                else:
+                    sell_quantity = int(sell_quantity)
+                    break
+            #go back to the previous command prompt if specified by the user
+            if go_back:
+                continue
+            #sell the given quantity of the stock. Print a success message and the cash balance
+            port.sell(sell_ticker, sell_quantity, market[sell_ticker])
+            print()
+            print(f"> {sell_quantity} shares of {sell_ticker} successfully sold for ${sell_quantity * market[sell_ticker]:.2f} <".center(80, "-"))
+            print()
+            print(f"Your cash balance is ${port.get_cash():.2f}\n".center(80))
+        elif command == "VIEW MARKET":
+            #display the stocks available to be traded
+            portfolio.view_market(market)
+            print()
+            print("> Market prices are updated after a command is entered <".center(80, "-"))
+            print()
+        elif command == "VIEW PORTFOLIO":
+            #display the user's portfolio
+            if len(port.portfolio) == 0:
+                print("You have no stocks in your portfolio")
+            else:
+                port.view_portfolio(market)
+            print()
+        elif command == "VIEW TRADES":
+            #display the user's trades
+            if len(port.trades) == 0:
+                print("You have no trades")
+            else:
+                port.view_trades()
+            print()
+        elif command == "VIEW CASH":
+            #display the user's cash balance
+            print(f"> Your cash balance is ${port.get_cash():.2f} <".center(80, "-"))
+            print()
+        elif command == "VIEW COMMANDS":
+            #display the list of valild program commands
+            portfolio.list_commands(portfolio_name)
+        elif command == "BACK":
+            print("! You are already at the base command prompt !\n")
+        elif command == "EXIT":
+            #exit the program after printing a goodbye message and the portfolio return
+            port.calculate_portfolio_value(market)
+            portfolio_return = (((port.portfolio_value + port.get_cash()) - cash) / cash) * 100
+            print("> Thank you for using the Terminal Portfolio Simulator <".center(80, "-"))
+            print()
+            print(f"Your {portfolio_name} portfolio returned {portfolio_return:.2f}%".center(80))
+            quit()
+        else:
+            print("! Invalid Command - Please Try Again !\n")
+
+
+if __name__ == "__main__":
+    main()
